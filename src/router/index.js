356cbd3bff2ce6d35068851d64492c50139129f0
Bhavin <bhavin1625p@gmail.com>
2022-02-02 20:09:58 +0530

    vuetify added to project

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
@@ -8,8 +7,13 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: Home
+        component: () => import('../views/Home.vue')
     },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: () => import('../views/Dashboard.vue')
+    }
 ];
 
 const router = new VueRouter({
