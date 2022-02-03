5e8389d0af99c03319bed0dce1747fe349f72d50
Bhavin <bhavin1625p@gmail.com>
2022-02-03 15:49:39 +0530

    build issue resolved

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/bhavinp16/weather_app_vue.git master:gh-pages
+git push -f https://github.com/bhavinp16/weatherApp-vue.git master:gh-pages
 cd -
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const routes = [
         path: '/',
         name: 'Home',
         component: () => import('../views/Home.vue')
+    },
+    {
+        path: '*',
+        name: 'catchAll',
+        component: () => import('../views/Home.vue')
     }
 ];
 
